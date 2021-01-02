ba00642176502cecaabaddca559c15e841118264
DESKTOP-RAEOFTC\ofeka <ofekaloush@gmail.com>
2021-01-02 18:58:45 +0200

    Q5 - Kfold for LogReg finished. still need to finish SVM

--- Naama/HW2.py
+++ Naama/HW2.py
@@ -7,16 +7,24 @@ import pandas as pd
 import numpy as np
 from pathlib import Path
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+import matplotlib.ticker as mticker
 import random
-import seaborn as sns
-from sklearn.preprocessing import LabelEncoder
-from sklearn import preprocessing
-from sklearn import metrics
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import roc_curve
+from sklearn.model_selection import train_test_split, GridSearchCV
+from sklearn.preprocessing import OneHotEncoder
+from sklearn import metrics
+from sklearn.metrics import roc_auc_score, confusion_matrix
+from sklearn.model_selection import StratifiedKFold
+from sklearn.pipeline import Pipeline
 from sklearn.ensemble import RandomForestClassifier as rfc
+from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVC
+import seaborn as sns
+##
+import scipy.stats as stats
+from sklearn.model_selection import StratifiedKFold as SKFold
+from sklearn import preprocessing
+from sklearn.metrics import roc_curve
 from sklearn.linear_model import Ridge
 from sklearn.linear_model import Lasso
 from sklearn.preprocessing import PolynomialFeatures
@@ -80,7 +88,14 @@ T1D_clean = nan2samp(T1D_dataset)
 
 # section 2
 lbl = np.ravel(T1D_clean['Diagnosis'])
-X_train, X_test, y_train, y_test = train_test_split(T1D_clean, lbl, test_size=0.2, random_state=0, stratify=lbl)
+X_train_tmp, X_test_tmp, y_train, y_test = train_test_split(T1D_clean, lbl, test_size=0.2, random_state=10, stratify=lbl)
+X_train = X_train_tmp.drop(columns=['Diagnosis'])
+X_test = X_test_tmp.drop(columns=['Diagnosis'])
+
+# lbl = np.ravel(T1D_clean['Diagnosis'])
+# X_train, X_test, y_train, y_test = train_test_split(T1D_clean, lbl, test_size=0.2, random_state=10, stratify=lbl)
+# X_train.drop(columns=['Diagnosis'], inplace=True)
+# X_test.drop(columns=['Diagnosis'], inplace=True)
 
 # Section 3.a - show that the distribution of the features is similar between test and train
 # Using a function to create a table of positive rates for every feature in the train/test groups:
@@ -88,11 +103,203 @@ from functions import dist_table as dist
 X_test_dummy = pd.get_dummies(X_test, dummy_na=False, drop_first=True)
 X_train_dummy = pd.get_dummies(X_train, dummy_na=False, drop_first=True)
 d_table = dist(X_train_dummy, X_test_dummy)
-print(d_table.transpose())
+d_table.transpose()
 
 # Section 3.b - show the relationship between feature and label:
 from functions import feat_lab_cor as fl_cor
-fl_cor(T1D_dataset)
+fl_cor(T1D_clean)
 
 # Section 3.c - additional plots
 #
+
+# Section 4 - One hot vectors encoding:
+# Encoding the data into one hot vectors while ignoring the 'Age' feature
+ohe = OneHotEncoder(sparse=False)
+# Encode X_train and X_test
+X_train_ohe = ohe.fit_transform(X_train.iloc[:, 1:])
+X_test_ohe = ohe.fit_transform(X_test.iloc[:, 1:])
+# Encode y_train and y_test and
+y_train_ohe = ohe.fit_transform(pd.DataFrame(y_train))
+y_test_ohe = ohe.fit_transform(pd.DataFrame(y_test))
+y_train_ohe_vec = y_train_ohe[:, 1]  # in the second column 0='Negative' and 1='Positive'
+y_test_ohe_vec = y_test_ohe[:, 1]
+
+# Section 5 - machine learning models
+# Section 5.a.i - linear models
+# Trying simple logistic regression:
+from functions import pred_log
+log_reg = LogisticRegression(solver='saga', multi_class='ovr', penalty='none', max_iter=10000, random_state=10)
+y_pred, w = pred_log(log_reg, X_train_ohe, y_train_ohe_vec, X_test_ohe)
+y_pred_p, w_p = pred_log(log_reg, X_train_ohe, y_train_ohe_vec, X_test_ohe, flag=True)
+print('AUROC with logistic regression is {:.3f}'.format(roc_auc_score(y_test_ohe_vec, y_pred_p[:, 1])))
+
+# 5K-cross fold - logistic regression
+from functions import kcfold
+C = np.array([0.01, 0.1, 1, 5, 10])  # regularization parameters
+K = 5  # number of folds
+penalty = ['l1', 'l2']  # types of penalties
+val_dict = kcfold(X_train_ohe, y_train_ohe_vec, C=C, penalty=penalty, K=K)
+
+# choosing the best parameters and predict:
+c = 5
+p = 'l2'
+log_reg_best = LogisticRegression(solver='saga', multi_class='ovr', penalty=p, C=c, max_iter=10000, random_state=10)
+y_pred_best, w_best = pred_log(log_reg_best, X_train_ohe, y_train_ohe_vec, X_test_ohe)
+y_pred_p_best, w_p_best = pred_log(log_reg_best, X_train_ohe, y_train_ohe_vec, X_test_ohe, flag=True)
+
+# display performance:
+print("evaluation metrics for logistic regression with 5K-cross fold validation:")
+print("F1 score is: " + str("{0:.2f}".format(100 * metrics.f1_score(pd.DataFrame(y_test_ohe_vec), pd.DataFrame(y_pred), average='macro'))) + "%")
+print("Acc is: " + str("{0:.2f}".format(100 * metrics.accuracy_score(pd.DataFrame(y_test_ohe_vec), pd.DataFrame(y_pred))) + "%"))
+print('AUROC is {:.3f}'.format(roc_auc_score(y_test_ohe_vec, y_pred_p_best[:, 1])))
+
+cnf_matrix = metrics.confusion_matrix(y_test_ohe_vec, y_pred_best)
+cnf_heat_map = sns.heatmap(cnf_matrix, annot=True, xticklabels=['Negative', 'Positive'], yticklabels=['Negative', 'Positive'])
+cnf_heat_map.set(ylabel='True labels', xlabel='Predicted labels')
+plt.show()
+
+# Section 5.a.ii - non linear models
+# 5K-cross fold - non linear SVM
+n_splits = K
+skf = StratifiedKFold(n_splits=n_splits, random_state=10, shuffle=True)
+svc = SVC(probability=True)
+C = np.array([1, 100, 1000])
+pipe = Pipeline(steps=[('scale', StandardScaler()), ('svm', svc)])
+svm_nonlin = GridSearchCV(estimator=pipe, param_grid={'svm__C': C, 'svm__kernel': ['rbf', 'poly'],
+                        'svm__gamma': ['auto', 'scale']}, scoring=['roc_auc'],
+                        cv=skf, refit='roc_auc', verbose=0, return_train_score=True)
+svm_nonlin.fit(X_train_ohe, y_train_ohe_vec)
+
+# Choose the best estimator and print
+best_svm_nonlin = svm_nonlin.best_estimator_
+print("Non-linear SVM best parameters are:")
+print(svm_nonlin.best_params_)
+
+# predict and calculate performance
+y_pred_test = best_svm_nonlin.predict(X_test_ohe)
+y_pred_proba_test = best_svm_nonlin.predict_proba(X_test_ohe)
+TN = confusion_matrix(y_test_ohe_vec, y_pred_test)[0, 0]
+FP = confusion_matrix(y_test_ohe_vec, y_pred_test)[0, 1]
+FN = confusion_matrix(y_test_ohe_vec, y_pred_test)[1, 0]
+TP = confusion_matrix(y_test_ohe_vec, y_pred_test)[1, 1]
+Se = TP/(TP+FN)
+Sp = TN/(TN+FP)
+PPV = TP/(TP+FP)
+NPV = TN/(TN+FN)
+Acc = (TP+TN)/(TP+TN+FP+FN)
+F1 = (2*PPV*Se)/(PPV+Se)
+
+# display performance:
+print("evaluation metrics for Non-linear SVM:")
+print(f'F1 is {F1:.2f}')
+print(f'Acc is {Acc:.2f}')
+print('AUROC is {:.3f}'.format(roc_auc_score(y_test_ohe_vec, y_pred_proba_test[:, 1])))
+
+cnf_matrix = metrics.confusion_matrix(y_test_ohe_vec, y_pred_test)
+cnf_heat_map = sns.heatmap(cnf_matrix, annot=True, xticklabels=['Negative', 'Positive'], yticklabels=['Negative', 'Positive'])
+cnf_heat_map.set(ylabel='True labels', xlabel='Predicted labels')
+plt.show()
+
+# Section 6 - feature selection
+# Random forest:
+rf_clf = rfc(random_state=10)
+rf_clf.fit(X_train_ohe, y_train_ohe_vec)
+y_pred = rf_clf.predict(X_test_ohe)
+y_pred_p_rf = rf_clf.predict_proba(X_test_ohe)
+
+print("evaluation metrics for random forest classifier:")
+print("Acc is: " + str("{0:.2f}".format(100 * metrics.accuracy_score(y_test_ohe_vec, y_pred))) + "%")
+print("F1 is: " + str("{0:.2f}".format(100 * metrics.f1_score(y_test_ohe_vec, y_pred, average='macro'))) + "%")
+print('AUROC is {:.3f}'.format(roc_auc_score(y_test_ohe_vec, y_pred_p_rf[:, 1])))
+
+cnf_matrix = metrics.confusion_matrix(y_test_ohe_vec, y_pred)
+cnf_heat_map = sns.heatmap(cnf_matrix, annot=True, xticklabels=['Negative', 'Positive'], yticklabels=['Negative', 'Positive'])
+cnf_heat_map.set(ylabel='True labels', xlabel='Predicted labels')
+plt.show()
+
+# Section 6.A.i - select 2 most important features according to the random forest:
+# Make a df of feature and importance
+features = rf_clf.feature_importances_
+feat_imp_dict = []
+i = 0
+for column in X_train.iloc[:, 1:].columns:
+    feat_imp_dict.append({"feature": column+" 0", "importance": features[i]})
+    feat_imp_dict.append({"feature": column+" 1", "importance": features[i+1]})
+    i += 2
+feat_imp = pd.DataFrame(feat_imp_dict)
+feature_names = np.array(feat_imp['feature'])
+
+sorted_idx = features.argsort()
+y_ticks = np.arange(0, len(features))
+fig, ax = plt.subplots()
+ax.barh(y_ticks, features[sorted_idx])
+ax.yaxis.set_major_locator(mticker.FixedLocator(y_ticks))
+ax.set_yticklabels(feature_names[sorted_idx])
+ax.set_yticks(y_ticks)
+ax.set_title("Random Forest Feature Importance")
+fig.tight_layout()
+plt.show()
+
+p=1
+
+
+
+#
+# 5K-cross fold - logistic regression visualization:
+# for d in val_dict:
+#     x = np.linspace(0, d['mu'] + 3 * d['sigma'], 1000)
+#     plt.plot(x, stats.norm.pdf(x, d['mu'], d['sigma']), label="p = " + d['penalty'] + ", C = " + str(d['C']))
+#     plt.title('Gaussian distribution of the loss')
+#     plt.xlabel('Average loss')
+#     plt.ylabel('Probability density')
+# plt.legend()
+# plt.show()
+
+# logistic regression with GreadSearchCV
+# logistic regression with k-cross fold using GridSearchCV
+# C_gs = np.array([0.0001, 0.001, 0.01, 1, 10, 100, 1000, 10000])
+# k = 5
+# SKF = SKFold(n_splits=k, random_state=10, shuffle=True)
+# log_reg_gs = LogisticRegression(random_state=5, max_iter=10000, solver='liblinear')
+# penalty = ['l1', 'l2']
+# #pipe = pipeline(steps=[('logistic', log_reg_gs)])
+# rf_clf = GridSearchCV(estimator=log_reg_gs, param_grid={'logistic__C': C_gs, 'logistic__penalty': penalty},
+#                    scoring=['roc_auc'], cv=SKF, refit='roc_auc', verbose=3, return_train_score=True)
+# rf_clf.fit(X_train_ohe, y_train_ohe_vec)
+
+# T1D_clean_dummy = pd.get_dummies(T1D_clean, drop_first=True)
+# label_encoder = LabelEncoder()
+# T1D_clean_val = T1D_clean.values
+# T1D_shape = T1D_clean_val.shape
+# T1D_lbl_encoded = np.zeros(T1D_shape)
+# for i in range(len(T1D_clean.columns)):
+#     if i == 1:
+#         T1D_lbl_encoded[:, i] = T1D_clean_val[:, i]
+#     else:
+#         T1D_lbl_encoded[:, i] = label_encoder.fit_transform(T1D_clean_val[:, i])
+
+# plot the performances
+# clf_type = ['rbf', 'scale']
+
+# calc_TN = lambda y_true, y_pred_1: confusion_matrix(y_true, y_pred)[0, 0]
+# calc_FP = lambda y_true, y_pred_1: confusion_matrix(y_true, y_pred)[0, 1]
+# calc_FN = lambda y_true, y_pred_1: confusion_matrix(y_true, y_pred)[1, 0]
+# calc_TP = lambda y_true, y_pred_1: confusion_matrix(y_true, y_pred)[1, 1]
+
+# plot_confusion_matrix(svm_nonlin, X_test_ohe, y_test_ohe_vec, cmap=plt.cm.Blues)
+# plt.show()
+
+
+#J_train = np.zeros((len(penalty), len(C)))
+#J_val = np.zeros((len(penalty), len(C)))
+
+# y_pred_train = log_reg_func.predict_proba(x_train)
+# J_train[penalty.index(p), np.where(C == c)] = log_loss(y_train, y_pred_train)
+# J_val[penalty.index(p), np.where(C == c)] = log_loss(y_val, y_pred_val)
+
+# plt.plot(1 / C, J_train[penalty.index(p), :])
+# plt.plot(1 / C, J_val[penalty.index(p), :])
+# plt.xlabel('C')
+# plt.ylabel('Loss')
+# plt.legend(['J_train (n = ' + str(x_train.shape[0]) + ')', 'J_val (n = ' + str(x_val.shape[0]) + ')'])
+# plt.show()
--- Naama/functions.py
+++ Naama/functions.py
@@ -5,7 +5,13 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 import numpy as np
+from sklearn.model_selection import StratifiedKFold as SKFold
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import log_loss
+from sklearn.metrics import roc_auc_score
+import random
 
+random.seed(10)
 
 def nancount(T1D_dataset):
     """
@@ -24,10 +30,11 @@ def nan2samp(T1D_dataset):
     :param T1D_dataset: Pandas series of T1D_dataset
     :return: A pandas dataframe of the dictionary T1D_d containing the "clean" features
     """
+    np.random.seed(10)
     T1D_d = {}
     T1D_dataset_temp = T1D_dataset.copy()
     for column in T1D_dataset_temp.columns:
-        col = T1D_dataset_temp[column]
+        col = T1D_dataset_temp[column].copy()
         i = col.isnull()  # create a boolean vector with true values where col has nan values
         idx = np.zeros(np.sum(i))
         t = 0  # initialize a counter for the nan locations vector (idx)
@@ -57,7 +64,7 @@ def dist_table(X_train,X_test):
         curr_test = x_test[column]
         train_prc = 100*(curr_train.sum()/curr_train.size)
         test_prc = 100*(curr_test.sum()/curr_test.size)
-        d_table[column] = {"train%": train_prc, "test%": test_prc, "delta%": train_prc - test_prc}
+        d_table[column] = {"Train %": train_prc, "Test %": test_prc, "Delta %": train_prc - test_prc}
     return pd.DataFrame(d_table)
 
 
@@ -65,21 +72,74 @@ def feat_lab_cor(T1D_dataset):
     """
     :param T1D_dataset: test df of T1D features
     """
-    fig, axes = plt.subplots(3, 6, figsize=(12, 6))
-    sns.set_context("paper", font_scale=0.7)
-    fig.suptitle('Relationships between features and labels')
+    fig, axes = plt.subplots(3, 6, figsize=(16, 10))
+    sns.set_context("paper", font_scale=0.8)
+    fig.suptitle('Relationships between features and labels', fontsize=14)
     i = 0
     for column in T1D_dataset:
         if column == 'Gender':
             feat_diag = sns.countplot(ax=axes[0, 0], x='Gender', hue='Diagnosis', data=T1D_dataset)
             feat_diag.set(xticklabels=['Male', 'Female'])
+            feat_diag.set(xlabel='', ylabel='')
+            feat_diag.set_title('Gender', fontsize=13)
         elif column == 'Age':
             feat_diag = sns.countplot(ax=axes[0, 1], x='Age', hue='Diagnosis', data=T1D_dataset)
             feat_diag.xaxis.set_major_locator(ticker.LinearLocator(10))
+            feat_diag.set(xlabel='', ylabel='')
+            feat_diag.set_title('Age', fontsize=13)
         else:
             if i < 18:
                 feat_diag = sns.countplot(ax=axes[i//6, i % 6], x=column, hue='Diagnosis', data=T1D_dataset)
                 feat_diag.set(xticklabels=['No', 'Yes'])
+                feat_diag.set(xlabel='',  ylabel='')
+                feat_diag.set_title(column, fontsize=13)
         i += 1
     plt.show()
     return()
+
+def pred_log(logreg, X_train, y_train, X_test, flag=False):
+    """
+    :param logreg: An object of the class LogisticRegression
+    :param X_train: Training set samples
+    :param y_train: Training set labels
+    :param X_test: Testing set samples
+    :param flag: A boolean determining whether to return the predicted he probabilities of the classes
+    :return: A two elements tuple containing the predictions and the weightning matrix
+    """
+
+    logreg.fit(X_train, y_train)
+    w_log = logreg.coef_
+    if flag == False:
+        y_pred_log = logreg.predict(X_test)
+    if flag == True:
+        y_pred_log = logreg.predict_proba(X_test)
+
+    return y_pred_log, w_log
+
+
+def kcfold(X, y, C, penalty, K):
+    """
+    :param X: Training set samples
+    :param y: Training set labels
+    :param C: A list of regularization parameters
+    :param penalty: A list of types of norm
+    :param K: Number of folds
+    :return: A dictionary as explained in the notebook
+    """
+    kf = SKFold(n_splits=K)
+    validation_dict = []
+    for p in penalty:
+        for c in C:
+            log_reg_func = LogisticRegression(solver='saga', penalty=p, C=c, max_iter=10000, multi_class='ovr', random_state=10)
+            loss_val_vec = np.zeros(K)
+            roc_auc_vec = np.zeros(K)
+            k = 0
+            for train_idx, val_idx in kf.split(X, y):
+                x_train, x_val = X[train_idx], X[val_idx]
+                y_train, y_val = y[train_idx], y[val_idx]
+                y_pred_val, w_pred_val = pred_log(log_reg_func, x_train, y_train, x_val, flag=True)
+                loss_val_vec[k] = log_loss(y_val, y_pred_val)
+                roc_auc_vec[k] = roc_auc_score(y_val, y_pred_val[:, 1])
+                k += 1
+            validation_dict.append({"C": c, "penalty": p, "mu": loss_val_vec.mean(), "sigma": loss_val_vec.std(), "roc_auc": roc_auc_vec.max()})
+    return validation_dict
